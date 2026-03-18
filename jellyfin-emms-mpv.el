6b2db3d3de62886c37a90053261cf02e12570bee
telecommuter <telecommuter@riseup.net>
2026-03-18 03:48:06 -0600

    emacs-os

--- jellyfin-emms-mpv.el
+++ jellyfin-emms-mpv.el
@@ -1,6 +1,6 @@
 ;;; jellyfin-emms-mpv.el --- Jellyfin API client for Emacs EMMS with mpv -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2026
+;; Copyright (C) 2026 emacs-os
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
